aabc50e47288ff89681206a0e1e5c5aca8ac9127
Emmy-Dare274 <emmy4toz2010@gmail.com>
2025-06-16 17:08:12 +0100

    Game function added to run Game

--- run.py
+++ run.py
@@ -9,11 +9,21 @@ guess_number = random.randint(1, 15)
 
 try:
     # Ask the user to guess a number
-    print("Welcome to the Number Guessing Game!")
+    print("Welcome to the Number Guessing Game. Have fun!")
     user_guess = int(input("Guess a number between 1 and 15:\n"))
     print(f'Your guess is: {user_guess}')
     print("Correct guess is:", guess_number)
 except Exception as e:
     print(f"An error occurred. Invalid input!")
-    print("Please enter a valid integer between 1 and 15.")
+    print("Please enter a valid number between 1 and 15.")
 
+# create a game function
+
+def game():
+    # create a variable to keep tract of the number of attempts guessed
+    attempts_guessed = 0
+    correct_guess = False
+
+    # Welcome the user to the game
+    print("Welcome to the Number Guessing Game. Have fun!")
+    
