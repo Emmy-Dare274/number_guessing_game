f154746fdf9fc8dabf67f33cfa28d3a106819582
Emmy-Dare274 <emmy4toz2010@gmail.com>
2025-06-16 13:02:04 +0100

    Random module generate from library and confirmed generating different numbers

--- run.py
+++ run.py
@@ -1,3 +1,8 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+# Import random module to generate a random number
+import random
+
+# declare a variable to generate a random number
+random_number = random.randint(1, 15)
+print("Random number generated:", random_number)
+
+
