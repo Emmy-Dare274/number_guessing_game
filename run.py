a443385ad231987eeadf134b01365aa35caf39ef
Emmy-Dare274 <emmy4toz2010@gmail.com>
2025-06-16 13:46:21 +0100

    Try and Except added to the game to check game operation

--- run.py
+++ run.py
@@ -2,7 +2,18 @@
 import random
 
 # declare a variable to generate a random number
-random_number = random.randint(1, 15)
-print("Random number generated:", random_number)
+guess_number = random.randint(1, 15)
+# print("Random number generated:", guess_number)
 
+# create a game function and the play again function
+
+try:
+    # Ask the user to guess a number
+    print("Welcome to the Number Guessing Game!")
+    user_guess = int(input("Guess a number between 1 and 15:\n"))
+    print(f'Your guess is: {user_guess}')
+    print("Correct guess is:", guess_number)
+except Exception as e:
+    print(f"An error occurred. Invalid input!")
+    print("Please enter a valid integer between 1 and 15.")
 
