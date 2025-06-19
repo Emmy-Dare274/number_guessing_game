bcb97e587d736f4db409a313faea81dae30974e4
Emmy-Dare274 <emmy4toz2010@gmail.com>
2025-06-19 17:42:30 +0100

    Game been refactored according to Mentor's advice

--- run.py
+++ run.py
@@ -1,12 +1,32 @@
 # Import random module to generate a random number
 import random
 
-# declare a variable to generate a random number
-# guess_number = random.randint(1, 15)
+''' This is a Python Number Guessing Game.
+This game generates a random number between 1 and 15,
+and prompts the user to guess the number.
+User tries to guess the number, and the game provides feedback. '''
 
-# create a function to play again
+def game_rule():
+    ''' This function prints the game rules to the user '''
+    print("\n" + "=" * 50)
+    print("Welcome to the Number Guessing Game. Have fun!")
+    print("NUMBER GUESSING GAME - RULES".center(50))
+    print ("=" * 50)
+    print("The rules are simple:")
+    print("1. I will think of a number between 1 and 15.")
+    print("2. You have to guess the number.")
+    print("3. I will give you hints if your guess is too high or too low.")
+    print("4. You must enter numbers only between 1 and 15.")
+    print("5. Repeating the same number will result in invalid attempts.")
+    print("6. Try to guess the number in as few attempts as possible.")
+    print("=" * 50 + "\n")
+
+game_rule()
 
 
+
+
+'''
 def play_again():
     # prompt the user to check if they want to play again
     response = input("Would you like to play again? (yes/no):\n")
@@ -64,3 +84,4 @@ def game():
 # Game function call
 
 game()
+'''
